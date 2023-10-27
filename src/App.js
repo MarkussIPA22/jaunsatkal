2d74c45b4e9d15466a8ff018186b5fa0b377deb2
MarkussIPA22 <125544038+MarkussIPA22@users.noreply.github.com>
2023-10-27 09:18:54 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import ToDo from './ToDo';
+import React, { useState } from 'react';
+
+function App(props) {
+const todo = {
+  userId: 1,
+  id: 1,
+  title: "delectus aut autem",
+  completed: false,
+};
+
+  return (
+    <div className="App">
+
+     <h1>Hello</h1>
+     <ToDo todo={todo}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/ToDo.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+function ToDo(props) {
+const [check , setCheck] = useState(props.todo.completed)
+
+function handleCheck(event) {
+    setCheck(event.target.checked)
+}
+return (
+<>
+
+<p>{props.todo.title}</p>
+<p>userID: {props.todo.userId}</p>
+<p>toDoId: {props.todo.id}</p>
+<input type="checkbox" checked={check} onChange={handleCheck} ></input>
+
+</>
+
+)
+
+}
+
+export default ToDo;
