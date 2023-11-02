76f6fed62cf27f08845a6f66bf056acec6a6f425
MarkussIPA22 <125544038+MarkussIPA22@users.noreply.github.com>
2023-11-02 11:34:51 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import ToDo from './ToDo';
-import React, { useState } from 'react';
+import { useEffect , useState } from "react"
 
 function App(props) {
-const todo = {
-  userId: 1,
-  id: 1,
-  title: "delectus aut autem",
-  completed: false,
-};
+
+const [todo, setToDo] = useState({});
+const [loading, setLoading] = useState(true);
+
+  useEffect( () => {
+    console.log("iet vai ne")
+    async function getData() {
+      const  response = await fetch("https://jsonplaceholder.typicode.com/todos/4")
+      const data = await response.json(); 
+      console.log(data)
+      setToDo(data)
+      setLoading(false)
+    }
+
+    getData()
+
+}, [])
+// const todo = {
+//   userId: 1,
+//   id: 1,
+//   title: "delectus aut autem",
+//   completed: false,
+// };
 
   return (
     <div className="App">
-
      <h1>Hello</h1>
-     <ToDo {...todo} />
+      { loading ? <p>Lādējas...</p>: <ToDo {...todo} />}
      
     </div>
   );
